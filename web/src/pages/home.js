eb57d601d4fbbd8c4c169d7b117018b2b8b47184
alami <alami@km.ru>
2021-08-13 23:32:15 +0500

    add Link from react-router-dom

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Home=() =>{
     return (
         <div>
             <h1>Notedly</h1>
             <p>This is the home page</p>
+            { /* добавьте список ссылок */ }
+            <ul>
+                <li>
+                    <Link to="/mynotes">My Notes</Link>
+                </li>
+                <li>
+                    <Link to="/favorites">Favorites</Link>
+                </li>
+            </ul>
+
         </div>
     );
 };
