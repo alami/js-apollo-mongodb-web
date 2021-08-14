4e09c0768c69becd94b4b4429ae9e9fa9824be1d
alami <alami@km.ru>
2021-08-14 18:34:51 +0500

    with pagination via cursor

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -33,8 +33,28 @@ const Home=() =>{
     // Если при получении данных произошел сбой, отображаем сообщение об ошибке
     if (error) return <p>Error!</p>;
     // Если загрузка данных произошла успешно, отображаем их в UI
-    return <NoteFeed notes={data.noteFeed.notes} />;
-
-};
+    return (
+<React.Fragment>
+    <NoteFeed notes={data.noteFeed.notes}/>
+    {/* Only display the Load More button if hasNextPage is true */}
+    {data.noteFeed.hasNextPage && (
+      // onClick выполняет запрос, передавая в качестве переменной текущий курсор
+  <Button onClick={() => fetchMore({
+    variables: { cursor: data.noteFeed.cursor},
+    updateQuery: (previousResult, {fetchMoreResult}) => {
+       return {
+          noteFeed: {
+            cursor: fetchMoreResult.noteFeed.cursor,
+            hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+            // Совмещаем новые результаты со старыми
+            notes: [
+                ...previousResult.noteFeed.notes,
+                ...fetchMoreResult.noteFeed.notes
+            ],
+            __typename: 'noteFeed'
+  } }; } } ) }>
+  Load more</Button>)}
+</React.Fragment>
+);};
 export default Home;
 
