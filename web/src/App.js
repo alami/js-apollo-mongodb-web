434534d601a5d930a16399f753b581f563722a62
alami <alami@km.ru>
2021-08-14 14:22:47 +0500

    wrap APP() in <ApolloProvider>

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Импортируем библиотеки Apollo Client
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+const uri = process.env.API_URI;  // Настраиваем API URI и кэш
+const cache = new InMemoryCache();
+const client = new ApolloClient({ // Настраиваем Apollo Client
+    uri,
+    cache,
+    connectToDevTools: true
+});
+
 // Импортируем глобальные стили
 import GlobalStyle from '/components/GlobalStyle';
 // Импортируем маршруты
@@ -8,10 +18,10 @@ import Pages from '/pages';
 
 const App=() =>{
     return (
-        <div>
-            <GlobalStyle />
+        <ApolloProvider client={client}>
+        <GlobalStyle />
             <Pages />
-        </div>
+        </ApolloProvider>
     );
 };
 
