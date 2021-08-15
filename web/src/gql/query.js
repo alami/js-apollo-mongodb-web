0f02e465ffd1fb934d9529b8fba8bd181134f968
alami <alami@km.ru>
2021-08-15 18:39:12 +0500

    add My Notes ,200

--- web/src/gql/query.js
+++ web/src/gql/query.js
@@ -33,9 +33,30 @@ const GET_NOTE = gql`
         }
     }
 `;
+
+const GET_MY_NOTES = gql`
+    query me {
+        me{
+            id
+            username
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`;
+
 const IS_LOGGED_IN = gql`
     {
         isLoggedIn @client
     }
 `;
-export { GET_NOTES, GET_NOTE, IS_LOGGED_IN };
+export { GET_NOTES, GET_NOTE, GET_MY_NOTES, IS_LOGGED_IN };
