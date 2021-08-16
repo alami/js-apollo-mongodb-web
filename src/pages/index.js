7ecd08bb0c08081213639c2b50860557b31068e3
alami <alami@km.ru>
2021-08-16 17:51:11 +0500

    fin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,27 @@
+
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+
 import Layout from '../components/Layout';
+
 
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
-import NotePage from './note';
+import Note from './note';
 import SignUp from './signup';
 import SignIn from './signin';
 import NewNote from './new';
 import EditNote from './edit';
 
-
-import { useQuery, gql } from '@apollo/client';
-import Note from "../../05-CRUD/pages/note";
 const IS_LOGGED_IN = gql`
     {
         isLoggedIn @client
     }
 `;
+
 
 const Pages = () => {
     return (
@@ -37,16 +39,15 @@ const Pages = () => {
         </Router>
     );
 };
-
 // Добавляем компонент PrivateRoute под компонентом 'Pages'
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const { loading, error, data } = useQuery(IS_LOGGED_IN);
-// Если данные загружаются, выводим сообщение о загрузке
+    // Если данные загружаются, выводим сообщение о загрузке
     if (loading) return <p>Loading...</p>;
-// Если при получении данных произошел сбой, выводим сообщение об ошибке
+    // Если при получении данных произошел сбой, выводим сообщение об ошибке
     if (error) return <p>Error!</p>;
-// Если пользователь авторизован, направляем его к запрашиваемому компоненту
-// В противном случае перенаправляем на страницу авторизации
+    // Если пользователь авторизован, направляем его к запрашиваемому компоненту
+    // В противном случае перенаправляем на страницу авторизации
     return(
         <Route
             {...rest}
