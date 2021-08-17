73a993716f2f7a832af33b2309b979f198d81683
alami <alami@km.ru>
2021-08-17 13:56:21 +0500

    add style to Text&Image

--- mobile/src/Main.js
+++ mobile/src/Main.js
@@ -3,8 +3,12 @@ import { Text, View, Image } from 'react-native';
 const Main=() =>{
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hello world!</Text>
-            <Image source={require('../assets/images/hello-world.jpg')} />
+            <Text style={{ color: '#0077cc', fontSize: 48, fontWeight: 'bold' }}>
+                Hello world!</Text>
+            <Image style={{
+                width: 320,
+                height: 240,
+            }} source={require('../assets/images/hello-world.jpg')} />
         </View>
     );
 };
