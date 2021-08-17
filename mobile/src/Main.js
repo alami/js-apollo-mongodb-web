8e7572c216190261867e3e4983e67db08df3164d
alami <alami@km.ru>
2021-08-17 17:33:54 +0500

    add apollo/client & config.js, upd Main.js

--- mobile/src/Main.js
+++ mobile/src/Main.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import Screens from './screens';
 
-const Main=() =>{
-    return <Screens />;
+// Импортируем библиотеки Apollo
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+// Импортируем конфигурацию среды
+import getEnvVars from '../config';
+const { API_URI } = getEnvVars();
+
+// Настраиваем API URI и кэш
+const uri = API_URI;
+const cache = new InMemoryCache();
+
+// Настраиваем Apollo Client
+const client = new ApolloClient({
+    uri,
+    cache
+});
+
+const Main=() => {
+    // Оборачиваем приложение в компонент высшего порядка ApolloProvider
+    return (
+        <ApolloProvider client={client}>
+            <Screens />
+        </ApolloProvider>
+    );
 };
 
 export default Main;
