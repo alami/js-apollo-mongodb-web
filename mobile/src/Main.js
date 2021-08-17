c075af5a1f4a245d4b76f497a7144cb208848363
alami <alami@km.ru>
2021-08-17 14:25:30 +0500

    update by StyleSheet

--- mobile/src/Main.js
+++ mobile/src/Main.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet  } from 'react-native';
 const Main=() =>{
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ color: '#0077cc', fontSize: 48, fontWeight: 'bold' }}>
-                Hello world!</Text>
-            <Image style={{
-                width: 320,
-                height: 240,
-            }} source={require('../assets/images/hello-world.jpg')} />
+        <View style={styles.container}>
+            <Text style={styles.h1}>Hello world!</Text>
+            <Text style={styles.paragraph}>This is my app</Text>
+            <Image style={styles.myimage} source={require('../assets/images/hello-world.jpg')} />
         </View>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    h1: {
+        fontSize: 48,
+        fontWeight: 'bold'
+    },
+    paragraph: {
+        marginTop: 24,
+        marginBottom: 24,
+        fontSize: 18
+    },
+    myimage: {
+        width: 320,
+        height: 240
+    }
+});
+
 export default Main;
