0d8e93f441def766a495a4bd171d756f64140556
alami <alami@km.ru>
2021-08-17 17:12:07 +0500

    with Note component

--- mobile/src/components/NoteFeed.js
+++ mobile/src/components/NoteFeed.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {FlatList, View, Text} from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import styled from 'styled-components/native';
+import Note from './Note';
 // Фиктивные данные
 const notes = [
-    {id: 0, content: 'Giant Steps'},
-    {id: 1, content: 'Tomorrow Is The Question'},
-    {id: 2, content: 'Tonight At Noon'},
-    {id: 3, content: 'Out To Lunch'},
-    {id: 4, content: 'Green Street'},
-    {id: 5, content: 'In A Silent Way'},
-    {id: 6, content: 'Lanquidity'},
-    {id: 7, content: 'Nuff Said'},
-    {id: 8, content: 'Nova'},
-    {id: 9, content: 'The Awakening'}
+    { id: 0, content: 'Giant Steps' },
+    { id: 1, content: 'Tomorrow Is The Question' },
+    { id: 2, content: 'Tonight At Noon' },
+    { id: 3, content: 'Out To Lunch' },
+    { id: 4, content: 'Green Street' },
+    { id: 5, content: 'In A Silent Way' },
+    { id: 6, content: 'Lanquidity' },
+    { id: 7, content: 'Nuff Said' },
+    { id: 8, content: 'Nova' },
+    { id: 9, content: 'The Awakening' }
 ];
 // Определение компонента в стиле FeedView
 const FeedView = styled.View`
@@ -20,7 +21,6 @@ const FeedView = styled.View`
   overflow: hidden;
   margin-bottom: 10px;
 `;
-// Добавляем компонент в стиле Separator (разделитель)
 const Separator = styled.View`
   height: 1;
   width: 100%;
@@ -35,7 +35,7 @@ const NoteFeed = props => {
                 ItemSeparatorComponent={() => <Separator />}
                 renderItem={({ item }) => (
                     <FeedView>
-                        <Text>{item.content}</Text>
+                        <Note note={item} />
                     </FeedView>
                 )}
             />
