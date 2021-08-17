95edf16cdc5297954f7433820a1e61904c23346b
alami <alami@km.ru>
2021-08-17 15:33:12 +0500

    Stack Navigation : button&back routing

--- mobile/src/screens/index.js
+++ mobile/src/screens/index.js
@@ -1,29 +1,45 @@
 import React from 'react';
+import { Text, View, ScrollView, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
 // Импортируем компоненты экрана
 import Feed from './feed';
 import Favorites from './favorites';
 import MyNotes from './mynotes';
-
+import NoteScreen from './note';
+// Стек навигации
+const FeedStack = createStackNavigator({
+    Feed: Feed,
+    Note: NoteScreen
+});
+const MyStack = createStackNavigator({
+    MyNotes: MyNotes,
+    Note: NoteScreen
+});
+const FavStack = createStackNavigator({
+    Favorites: Favorites,
+    Note: NoteScreen
+});
+// Вкладки навигации
 const TabNavigator = createBottomTabNavigator({
     FeedScreen: {
-        screen: Feed,
+        screen: FeedStack,
         navigationOptions: {
-            tabBarLabel: 'Feed',
+            tabBarLabel: 'Feed'
         }
     },
     MyNoteScreen: {
-        screen: MyNotes,
+        screen: MyStack,
         navigationOptions: {
-            tabBarLabel: 'My Notes',
+            tabBarLabel: 'My Notes'
         }
     },
     FavoriteScreen: {
-        screen: Favorites,
+        screen: FavStack,
         navigationOptions: {
-            tabBarLabel: 'Favorites',
+            tabBarLabel: 'Favorites'
         }
     }
 });
