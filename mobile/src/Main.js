d953454b6d72660ff5151946e4325e6c747802e2
alami <alami@km.ru>
2021-08-17 13:09:54 +0500

    mofile-React-Native 1t app

---
+++ mobile/src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+const Main=() =>{
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Hello world!</Text>
+            <Image source={require('../assets/images/hello-world.jpg')} />
+        </View>
+    );
+};
+export default Main;
