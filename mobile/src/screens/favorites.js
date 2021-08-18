9856cded877989ddec6b0ce7ac4c292ab85b902f
alami <alami@km.ru>
2021-08-18 14:32:33 +0500

    mynotes & favorites with auth 299

--- mobile/src/screens/favorites.js
+++ mobile/src/screens/favorites.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useQuery, gql } from '@apollo/client';
 
-const Favorites = () => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Favorites</Text>
-        </View>
-    );
+import NoteFeed from '../components/NoteFeed';
+import Loading from '../components/Loading';
+
+// Наш GraphQL-запрос
+const GET_MY_FAVORITES = gql`
+    query me {
+        me{
+            id
+            username
+            favorites {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`;
+
+const Favorites = props => {
+    const { loading, error, data } = useQuery(GET_MY_FAVORITES);
+    // Если данные загружаются, выдаем сообщение о загрузке
+    if (loading) return <Loading />;
+    // Если при получении данных произошел сбой, выдаем сообщение об ошибке
+    if (error) return <Text>Error loading notes</Text>;
+    // Если запрос выполнен успешно и содержит заметки, возвращаем их в ленту
+    // Если запрос выполнен успешно, но заметок не содержит, отображаем сообщение
+    if (data.me .favorites.length !== 0) {
+        return <NoteFeed notes={data.me.favorites} navigation={props.navigation} />;
+    }else{
+        return <Text>No notes yet</Text>;
+    }
 };
+
 Favorites.navigationOptions = {
     title: 'Favorites'
 };
--- mobile/src/screens/mynotes.js
+++ mobile/src/screens/mynotes.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { useQuery, gql } from '@apollo/client';
+import NoteFeed from '../components/NoteFeed';
+import Loading from '../components/Loading';
 
-const MyNotes = () => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>My Notes</Text>
-        </View>
-    );
+// Наш GraphQL-запрос
+const GET_MY_NOTES = gql`
+    query me {
+        me{
+            id
+            username
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`;
+
+const MyNotes = props => {
+    const { loading, error, data } = useQuery(GET_MY_NOTES);
+// Если данные загружаются, приложение будет выводить сообщение о загрузке
+    if (loading) return <Loading />;
+// Если при получении данных произошел сбой, выводим сообщение об ошибке
+    if (error) return <Text>Error loading notes</Text>;
+// Если запрос выполнен успешно и содержит заметки, возвращаем их в ленту
+// Если же запрос выполнен успешно, но заметок не содержит, отображаем
+// сообщение
+    if (data.me .notes.length !== 0) {
+        return <NoteFeed notes={data.me.notes} navigation={props.navigation} />;
+    }else{
+        return <Text>No notes yet</Text>;
+    }
 };
+
 MyNotes.navigationOptions = {
     title: 'My Notes'
 };
