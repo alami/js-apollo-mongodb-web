caa70eeeb8cd5271f0677d0a65528bd45c92170f
alami <alami@km.ru>
2021-08-18 12:05:20 +0500

    ch24 : authloading - only Loading indicator

--- mobile/src/screens/index.js
+++ mobile/src/screens/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, ScrollView, Button } from 'react-native';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -10,6 +10,14 @@ import Feed from './feed';
 import Favorites from './favorites';
 import MyNotes from './mynotes';
 import NoteScreen from './note';
+
+import AuthLoading from './authloading';
+import SignIn from './signin';
+import Settings from './settings';
+
+const AuthStack = createStackNavigator({
+    SignIn: SignIn
+});
 // Стек навигации
 const FeedStack = createStackNavigator({
     Feed: Feed,
@@ -23,6 +31,11 @@ const FavStack = createStackNavigator({
     Favorites: Favorites,
     Note: NoteScreen
 });
+
+const SettingsStack = createStackNavigator({
+    Settings: Settings
+});
+
 // Вкладки навигации
 const TabNavigator = createBottomTabNavigator({
     FeedScreen: {
@@ -52,7 +65,27 @@ const TabNavigator = createBottomTabNavigator({
                 <MaterialCommunityIcons name="star" size={24} color={tintColor}/>
             )
         }
+    },
+    Settings: {
+        screen: Settings,
+        navigationOptions: {
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ tintColor }) => (
+                <MaterialCommunityIcons name="settings" size={24} color={tintColor} />
+            )
+        }
     }
 });
+const SwitchNavigator = createSwitchNavigator(
+    {
+        AuthLoading: AuthLoading,
+        Auth: AuthStack,
+        App: TabNavigator
+    },
+    {
+        initialRouteName: 'AuthLoading'
+    }
+);
+
 // Создаем контейнер приложения
-export default createAppContainer(TabNavigator);
+export default createAppContainer(SwitchNavigator);
